115343c38f37fecd150f9f9a724204e677ca1706
Lilia Brown <160168216+Lilia-Brown@users.noreply.github.com>
2025-05-27 20:24:47 -0700

    [Feat] Adds middleware to budget routes

--- routes/budgetRoutes.js
+++ routes/budgetRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const authMiddleware = require('../middleware/authMiddleware');
+
 
 const router = express.Router();
+router.use(authMiddleware);
 
 module.exports = (prisma) => {
   // GET / - Get all budgets
